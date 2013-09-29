a4273b213cf6333e9de5b051cb45ef56d61a13df
Brian Han <brianhan87usa@gmail.com>
2013-09-29 16:04:29 -0400

    Generate Goal model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 BourbonPractice::Application.routes.draw do
+  root "goals#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
