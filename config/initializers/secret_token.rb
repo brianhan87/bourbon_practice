a62e88f178f2a7d75a940bb0aee1675d23d5f220
Brian Han <brianhan87usa@gmail.com>
2013-09-29 15:45:23 -0400

    Install Bourbon & Bourbon Neat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BourbonPractice::Application.config.secret_key_base = '91464cf0877333a0f371bf27a2fffa0bf965ce5e5a71fc4ed7288497ab34926d7a0e8db2615ff4c0313485fd9c647c71033cc2a2626e4ce6aaa88e912f9a76a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BourbonPractice::Application.config.session_store :cookie_store, key: '_bourbon_practice_session'
